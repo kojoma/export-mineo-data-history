aad97dd78973f805e8634ae3d529d959b659e6f4
kojoma <masaru.kojo@gmail.com>
2019-07-15 19:10:49 +0900

    Prepare data format for output csv

--- contentScript.js
+++ contentScript.js
@@ -2,11 +2,15 @@ findElement();
 
 function findElement() {
   $(function() {
+    var dataHistoryArray = [];
+
     let tabBox = $('body').find('#tabbox');
     let tabList = tabBox.find('p').find('a');
     tabBox.find('div').each(function (monthIndex) {
+      var monthName = '';
       if (monthIndex < tabList.length) {
-        console.log($(tabList[monthIndex]).text().trim());
+        monthName = $(tabList[monthIndex]).text().trim();
+        console.log(monthName);
       } else {
         // Skip this loop
         return true;
@@ -14,40 +18,42 @@ function findElement() {
 
       $(this).find('tbody').find('tr').each(function (trIndex) {
         if (shouldOutput($(this))) {
-          var output = '';
+          var output = [formatMonth(monthName)];
 
           // xx日
-          output = $(this).find('th').text().trim();
+          output.push(formatDay($(this).find('th').text()));
 
           // xxMB
           $(this).find('td').each(function (_) {
-            output = buildOutputString(output, $(this).text());
+            output.push(formatData($(this).text()));
           });
           console.log(output);
-          output = '';
+
+          dataHistoryArray.push(output);
+          output = [];
         }
       });
     });
   });
-}
 
-function shouldOutput(trElement) {
-  if (trElement.find('th').length === 1) {
-    let daysDataElement = trElement.find('td').not('.bk_color_sum, .bk_color_ave');
-    return daysDataElement.length !== 0;
-  } else {
-    return false;
-  }
-}
-
-function buildOutputString(nowString, additionalString) {
-  var newString = '';
-
-  if (nowString.length === 0) {
-    newString = nowString + additionalString.trim();
-  } else {
-    newString = nowString + ', ' + additionalString.trim();
+  function shouldOutput(trElement) {
+    if (trElement.find('th').length === 1) {
+      let daysDataElement = trElement.find('td').not('.bk_color_sum, .bk_color_ave');
+      return daysDataElement.length !== 0;
+    } else {
+      return false;
+    }
   }
 
-  return newString;
+  function formatMonth(monthString) {
+    return monthString.replace('月', '').trim();
+  }
+
+  function formatDay(dayString) {
+    return dayString.replace('日', '').trim();
+  }
+
+  function formatData(dataString) {
+    return dataString.replace('MB', '').replace(',', '').trim();
+  }
 }
