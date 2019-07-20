f65a440817fb36f68f5e108b306997848a7b0ad0
kojoma <masaru.kojo@gmail.com>
2019-07-20 17:06:42 +0900

    Refactor for prepare download csv

--- contentScript.js
+++ contentScript.js
@@ -1,59 +1,58 @@
-findElement();
+var allHistory = {};
+fetchDataHistory();
+console.log(allHistory);
 
-function findElement() {
-  $(function() {
-    var dataHistoryArray = [];
-
-    let tabBox = $('body').find('#tabbox');
-    let tabList = tabBox.find('p').find('a');
-    tabBox.find('div').each(function (monthIndex) {
-      var monthName = '';
-      if (monthIndex < tabList.length) {
-        monthName = $(tabList[monthIndex]).text().trim();
-        console.log(monthName);
-      } else {
-        // Skip this loop
-        return true;
-      }
-
-      $(this).find('tbody').find('tr').each(function (trIndex) {
-        if (shouldOutput($(this))) {
-          var output = [formatMonth(monthName)];
-
-          // xx日
-          output.push(formatDay($(this).find('th').text()));
-
-          // xxMB
-          $(this).find('td').each(function (_) {
-            output.push(formatData($(this).text()));
-          });
-          console.log(output);
-
-          dataHistoryArray.push(output);
-          output = [];
-        }
-      });
-    });
-  });
-
-  function shouldOutput(trElement) {
-    if (trElement.find('th').length === 1) {
-      let daysDataElement = trElement.find('td').not('.bk_color_sum, .bk_color_ave');
-      return daysDataElement.length !== 0;
+function fetchDataHistory() {
+  let tabBox = $('body').find('#tabbox');
+  let tabList = tabBox.find('p').find('a');
+  tabBox.find('div').each(function (monthIndex) {
+    var monthName = '';
+    if (monthIndex < tabList.length) {
+      monthName = $(tabList[monthIndex]).text().trim();
     } else {
-      return false;
+      // Skip this loop
+      return true;
     }
-  }
 
-  function formatMonth(monthString) {
-    return monthString.replace('月', '').trim();
-  }
+    var historyByMonth = [];
 
-  function formatDay(dayString) {
-    return dayString.replace('日', '').trim();
-  }
+    $(this).find('tbody').find('tr').each(function () {
+      if (shouldOutput($(this))) {
+        var historyByDay = [];
 
-  function formatData(dataString) {
-    return dataString.replace('MB', '').replace(',', '').trim();
+        // xx日
+        historyByDay.push(formatDay($(this).find('th').text()));
+
+        // xxMB
+        $(this).find('td').each(function (_) {
+          historyByDay.push(formatData($(this).text()));
+        });
+
+        historyByMonth.push(historyByDay);
+      }
+    });
+
+    allHistory[formatMonth(monthName)] = historyByMonth;
+  });
+}
+
+function shouldOutput(trElement) {
+  if (trElement.find('th').length === 1) {
+    let daysDataElement = trElement.find('td').not('.bk_color_sum, .bk_color_ave');
+    return daysDataElement.length !== 0;
+  } else {
+    return false;
   }
+}
+
+function formatMonth(monthString) {
+  return monthString.replace('月', '').trim();
+}
+
+function formatDay(dayString) {
+  return dayString.replace('日', '').trim();
+}
+
+function formatData(dataString) {
+  return dataString.replace('MB', '').replace(',', '').trim();
 }
